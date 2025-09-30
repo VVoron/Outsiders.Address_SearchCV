e6cb0f873e29b92a03ce5ad3a7a43b478ea76918
heaven <shmarev082@gmail.com>
2025-10-01 00:43:09 +0300

    chore: add .gitignore and remove build artifacts

--- GeoClipService/Models/Response.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GeoClipService.Models;
-
-public class Response
-{
-    public IList<string>? Jobs { get; set; }
-    public IList<ValidationErrorResponse>? ValidationErrors { get; set; }
-}
--- GeoClipService/Models/ValidationErrorDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GeoClipService.Models;
-
-public class ValidationErrorResponse
-{
-    public string TaskId { get; set; }
-    public string Error { get; set; }
-}
