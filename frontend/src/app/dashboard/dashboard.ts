1876f2668ca78d42325a86bad4eb233795b4eb6b
heaven <shmarev082@gmail.com>
2025-10-19 04:24:24 +0300

    remove old frontend

--- frontend/src/app/dashboard/dashboard.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  template: `<h2>Добро пожаловать!</h2>`,
-})
-export class DashboardComponent {}
